6dbf62202696fb27e5d35af244497d5437c4ab52
Andrew Dunham <andrew@du.nham.ca>
2014-02-11 20:10:14 -0800

    Optimistically add new items, deleting on error

--- src/js/EditBox.jsx
+++ src/js/EditBox.jsx
@@ -9,13 +9,23 @@ var EditBox = React.createClass({
         var title = this.refs.title.getDOMNode().value.trim();
         var text = this.refs.text.getDOMNode().value.trim();
 
-        var model = new NoteItem({
+        var coll = this.props.coll;
+
+        // Optimistically create and add the model, and, if the save fails,
+        // back out the change.
+        // TODO: report this error to the user rather than have the item just
+        // mysteriously disappear... maybe have a 'saved' flag on the model?
+        var model = coll.create({
             type: 'note',
             title: title,
             text: text
+        }, {
+            error: function(model, xhr, options) {
+                // This is only triggered if the model failed to save.  If so,
+                // we remove this model from the collection.
+                coll.remove(model);
+            },
         });
-        this.props.coll.add(model);
-        model.save();
 
         return false;
     },
