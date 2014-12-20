f6de6fe57d4936a01b8af73eafaae539be443d63
Andrew Dunham <andrew@du.nham.ca>
2014-12-19 17:56:16 -0800

    Add note for indexing

--- index.go
+++ index.go
@@ -4,6 +4,9 @@ import (
 	"github.com/blevesearch/bleve"
 )
 
+// TODO: actually index things
+// https://github.com/piger/spock/blob/26c6044c4f239794532d2ec10499c645779c5d39/index.go
+
 func buildIndexMapping() *bleve.IndexMapping {
 	stdTextMapping := bleve.NewTextFieldMapping()
 	stdTextMapping.Analyzer = "standard"
