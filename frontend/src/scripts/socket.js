0d1a4f1c22d561ca65f4a537bef39d808ff5a9a0
Andrew Dunham <andrew@du.nham.ca>
2015-01-06 18:39:18 -0800

    Use reconnecting socket

--- frontend/src/scripts/socket.js
+++ frontend/src/scripts/socket.js
@@ -1,102 +1,122 @@
-var // path = window.location.protocol + '//' + window.location.host;
-    path = '/api/sockjs',
+var path = '/api/sockjs',
     extend = require('lodash-node/modern/objects/assign'),
     events = require('events'),
-    sockjs = require('sockjs-client');
+    SockJS = require('sockjs-client');
 
 
-// The backoff wait times, in seconds.
-var BACKOFF_WAITS = {
-    1:   2,
-    2:   5,
-    3:  10,
-    4:  20,
-    5:  30,
-    6:  60,
-    7: 120,
-};
+class ReconnectingSocket {
+    constructor(cli_path, options, status_cb, cli_onmessage, cli_onopen, cli_onclose) {
+        this.cli_path = cli_path;
+        this.status_cb = status_cb;
+        this.cli_onmessage = cli_onmessage;
+        this.cli_onopen = cli_onopen;
+        this.cli_onclose = cli_onclose;
 
-// Default options.
-var DEFAULT_OPTIONS = {
-    // Whether or not we should reconnect
-    reconnect: true,
-
-    // TODO: more
-};
-
-
-class ReconnectingSocket extends events.EventEmitter {
-    constructor(path, options) {
-        super();
-
-        // Private variables.
-        this._path = path;
-        this._conn = null;
-        this._reconnecting = false;
-
-        // Public variables
-        this.options = extend({}, DEFAULT_OPTIONS, options);
-    }
-
-    connect() {
-        if( this._conn ) {
-            this._conn.close();
-            this._conn = null;
-        }
-
-        this._conn = sockjs(path);
-        this.emit('status', 'connecting');
-
-        // Setup callbacks.
-        this._conn.onopen = this.on_open.bind(this);
-        this._conn.onclose = this.on_close.bind(this);
-        this._conn.onmessage = this.on_message.bind(this);
-    }
-
-    on_open() {
-        this.reset_reconnect();
-        this.update_status();
-        this.emit('open');
-    }
-
-    on_close() {
-        this._conn = null;
-        this.update_status();
-
-        this.emit('close');     // TODO: use "disconnecting"?
-        if( !this.options.reconnect ) {
-            return;
-        }
-
-        this.try_reconnect(this.connect.bind(this));
-    }
-
-    on_message(e) {
-        this.emit('message', e.data);
-    }
-
-    reset_reconnect() {
-        this._reconnecting = false;
-        // TODO: reset reconnect count
-    }
-
-    try_reconnect(connfunc) {
-        // TODO
+        this.reconnect = {
+            reconnecting:          false,
+            do_not_reconnect:      false,
+            reload_after_n:        true,
+            max_retries:           30,
+            reset_mult:            6,
+            retry_timeout_ms:      1500 + Math.floor(Math.random() * 60),
+            retry_multiplier:      2,
+            retry_curr_multiplier: 0,
+            retry_curr_timeout:    0,
+            retry_count:           0,
+        };
+        extend(this.reconnect, options)
     }
 
     update_status() {
-        if( this._reconnecting ) {
-            this.emit('reconnecting');
-        } else if( this._conn === null || this._conn !== SockJS.OPEN ) {
-            this.emit('disconnected');
+        if( this.status_cb === null ) {
+            return;
+        }
+
+        if( this.reconnect.reconnecting ) {
+            this.status_cb('reconnecting');
+        } else if( this.conn === null || this.conn.readyState !== SockJS.OPEN ) {
+            this.status_cb('disconnected');
         } else {
-            this.emit('connected');
+            this.status_cb('connected');
         }
     }
 
-    // Public functions.
+    connect() {
+        if( this.conn != null ) {
+            this.conn.close();
+            this.conn = null;
+        }
+
+        this.conn = new SockJS(this.cli_path);
+        if( this.status_cb != null ) {
+            this.status_cb('connecting');
+        }
+
+        this.conn.onopen = this.on_open;
+        this.conn.onclose = this.on_close;
+        this.conn.onmessage = this.cli_onmessage;
+    }
+
+    reconnect_reset() {
+        this.reconnect.reconnecting = false;
+        this.reconnect.retry_curr_timeout = 0;
+        this.reconnect.retry_curr_multipler = 0;
+        this.reconnect.retry_count = 0;
+    }
+
+    reconnect_try(connfunc) {
+        if( this.reconnect.retry_count === this.reconnect.max_retries ) {
+            this.reconnect.reconnecting = false;
+            if( this.reconnect.reload_after_n ) {
+                window.location.reload(true);
+            }
+
+            return;
+        }
+        if( !this.reconnect.reconnecting ) {
+            this.reconnect.reconnecting = true;
+            this.reconnect.retry_curr_timeout = this.reconnect.retry_timeout_ms;
+            this.reconnect.retry_curr_multipler = 1;
+            this.reconnect.retry_count = 1;
+            connfunc();
+        } else {
+            this.reconnect.retry_count += 1;
+            var callback = () => {
+                this.reconnect.retry_curr_timeout *= this.reconnect.retry_multiplier;
+                this.reconnect.retry_curr_multipler += 1;
+                if( this.reconnect.retry_curr_multipler === this.reconnect.reset_mult ) {
+                    this.reconnect.retry_curr_timeout = this.reconnect.retry_timeout_ms;
+                    this.reconnect.retry_curr_multipler = 1;
+                }
+                connfunc();
+            };
+
+            setTimeout(callback, this.reconnect.retry_curr_timeout);
+        }
+    }
+
+    on_open() {
+        this.reconnect_reset();
+        this.update_status();
+        if( this.cli_onopen != null ) {
+            return this.cli_onopen();
+        }
+    }
+
+    on_close() {
+        this.conn = null;
+        this.update_status();
+        if( this.cli_onclose != null ) {
+            this.cli_onclose();
+        }
+        if( this.reconnect.do_not_reconnect ) {
+            return;
+        }
+        this.reconnect_try(this.connect);
+    }
+
     send(data) {
-        this._conn.send(data);
+        return this.conn.send(data);
     }
 
     sendMessage(ty, obj) {
@@ -104,55 +124,7 @@ class ReconnectingSocket extends events.EventEmitter {
     }
 }
 
-
-// Create the underlying SockJS instance.
-var sockjs = sockjs(path);
-
-// The current backoff count.
-var backoffCount;
-
-// The token for our current backoff timeout.
-var backoffToken = null;
-
-// The backoff function.
-var backoffRetry = function() {
-    // Try reconnecting again
-    // TODO:
-
-    // Increment backoff counter.
-    backoffToken = setTimeout(backoffRetry, BACKOFF_WAITS[++backoffCount] * 1000);
-};
-
-// When we're disconnected, try reconnecting immediately, and start our backoff
-// timer.
-socket.onclose = function() {
-    console.log("Disconnected from server");
-
-    // Set our backoff count and start waiting.
-    backoffCount = 1;
-    backoffToken = setTimeout(backoffRetry, BACKOFF_WAITS[backoffCount] * 1000);
-
-    // TODO: call onclose callback
-};
-
-// When we reconnect, stop our backoff counter.
-socket.onopen = function() {
-    console.log("Connected to server");
-
-    backoffCount = 0;
-    if( backoffToken ) {
-        clearTimeout(backoffToken);
-        backoffToken = null;
-    }
-
-    // TODO: call onopen callback
-};
-
-/*
- * Helper method that sends a message type, and then a JSON-encoded object.
- */
-socket.sendMessage = function(ty, obj) {
-    return this.send(ty + "|" + JSON.stringify(obj));
-};
+var socket = new ReconnectingSocket(path);
+socket.connect();
 
 module.exports = socket;
